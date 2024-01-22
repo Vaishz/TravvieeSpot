c7ef2a4e511a1dab68f38bc18d90015f3babd205
yepvaishz <poti.vaishnavi21@gmail.com>
2024-01-22 20:01:21 +0530

    adding evertyh

---
+++ TravieSpot/src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+
+
+const App = () => {
+  return (
+    <>
+    <BrowserRouter>
+    <Routes>
+      <Route path = '/' element = {<Home/>}/>
+      <Route path = '/' element = {<Home/>}/>
+      <Route path = '/' element = {<Home/>}/>
+      <Route path = '/' element = {<Home/>}/>
+      <Route path = '/' element = {<Home/>}/>
+      <Route path = '/' element = {<Home/>}/>
+      <Route path = '/' element = {<Home/>}/>
+      <Route path = '/' element = {<Home/>}/>
+
+    </Routes>
+    </BrowserRouter>
+    
+    
+    
+    </>
+  )
+}
+
+export default App
